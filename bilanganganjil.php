0cde7393fbefe44dbdf60ebaadc90fedc782a5ea
Firya Syafira <55348258+firyasyafira@users.noreply.github.com>
2019-09-29 20:27:39 +0700

    Add files via upload

---
+++ bilanganganjil.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Bilangan Ganjil</title>
+</head>
+<body>
+<h2>Bilangan Ganjil</h2>
+<?php 
+for ($ganjil=1; $ganjil <= 100 ; $ganjil++) { 
+	if ($ganjil %2 == 1) {
+		echo "$ganjil";
+		echo " ";
+	}
+}
+ ?>
+</body>
+</html>
+
